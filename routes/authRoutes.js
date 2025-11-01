d6de98fcb188d123129906b31996787678ea294f
adityasathawane <adityasathawane2003@gmail.com>
2025-11-01 22:11:43 +0530

    Routes added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,4 +4,4 @@ import { registerUser, loginUser } from "../controllers/authController.js";
 const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-export default router;
+export default router; // auth routes
